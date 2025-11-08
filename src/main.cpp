a1f2e9524526d55a54e488d4bd7f499b89e6a127
Mahdi El husseini <melhusse@ecelnx51.ecn.purdue.edu>
2025-11-08 16:11:04 -0500

    pins initialized

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
 #include "pico/stdlib.h"
+#include "quaternary.h"
+#include "ternary.h"
+#include "binary.h"
 #include "unary.h"
+
 using namespace std;
 
 /* Functions used in main.cpp */
@@ -14,8 +18,9 @@ void config_outputs(vector<uint32_t> pins) {
     for (auto currPin : pins) {
         gpio_init(currPin); // initialize GPIO
         gpio_set_dir(currPin, GPIO_OUT);  // set to output
-        
-        sio_hw->gpio_clr = currPin; // turn pin off !!
+        sleep_ms(10);
+
+        gpio_put(currPin, false);
     }
 }
 
@@ -24,14 +29,14 @@ void config_inputs(vector<uint32_t> pins) {
         gpio_init(currPin);
         gpio_set_dir(currPin, GPIO_IN);
 
-        sio_hw->gpio_clr = currPin; // turn pin off
+        gpio_put(currPin, false); // turn pin off
     }
 }
 int main(void) {
 
     // configure GPIO pins
-    vector<uint32_t> outputs = {22, 23, 24, 25};    // use these pins as outputs
-    vector<uint32_t> inputs = {};
+    vector<uint32_t> outputs = {22, 23, 24, 25, OUTPUT_PIN1, OUTPUT_PIN2, OUTPUT_PIN3, OUTPUT_PIN4};    // use these pins as outputs
+    vector<uint32_t> inputs = {INPUT_PIN};
     config_outputs(outputs);
     config_inputs(inputs);
 }
